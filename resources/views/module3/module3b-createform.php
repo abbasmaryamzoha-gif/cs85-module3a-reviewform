a9f7dd6fc8281f5e6193f7568266bf3aa593333d
abbasmaryamzoha-gif <abbas_maryam_zoha@student.smc.edu>
2026-07-09 07:56:50 -0700

    made unique ids for each field and renamed them

--- resources/views/module3/module3b-createform.php
+++ resources/views/module3/module3b-createform.php
@@ -1,27 +1,31 @@
 
 <form action="" method="GET">
-  <label for="color">Favorite Color:</label>
-  <input type="text" id="color" name="color" required><br>
+  <label for="name">Full Name:</label>
+  <input type="text" id="name" name="name" required><br>
   
-  <label for="movie">Favorite Movie:</label>
-  <input type="text" id="movie" name="movie" required><br>
+  <label for="email">Email Address:</label>
+  <input type="text" id="email" name="email" required><br>
 
-  <label for="movie">Favorite Movie:</label>
-  <input type="text" id="movie" name="movie" required><br>
+  <label for="topic">Topic of Message (something meaningful to you):</label>
+  <input type="text" id="topic" name="topic" required><br>
 
-  <label for="movie">Favorite Movie:</label>
-  <input type="text" id="movie" name="movie" required><br>
+  <label for="message">Message (textarea, 50–150 words required):</label>
+  <input type="text" id="message" name="message" required><br>
   
-  <input type="submit" value="Submit Survey">
+  <input type="submit" value="Submit">
 </form>
-    
+  
+
+
+
+
+
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && !empty($_GET)) {
-  echo "<h3>Survey Results</h3>";
-  echo "<p>Favorite Color: " . htmlspecialchars($_GET['color']) . "</p>";
-  echo "<p>Favorite Movie: " . htmlspecialchars($_GET['movie']) . "</p>";
-  echo "<p>Favorite Color: " . htmlspecialchars($_GET['color']) . "</p>";
-  echo "<p>Favorite Movie: " . htmlspecialchars($_GET['movie']) . "</p>";
-
+  echo "<h3>your message:</h3>";
+  echo "<p>Full Name: " . htmlspecialchars($_GET['name']) . "</p>";
+  echo "<p>Email Address: " . htmlspecialchars($_GET['email']) . "</p>";
+  echo "<p>Topic of Message: " . htmlspecialchars($_GET['topic']) . "</p>";
+  echo "<p>Message:" . htmlspecialchars($_GET['message']) . "</p>";
 }
 ?>
