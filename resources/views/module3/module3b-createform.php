71de7e28465dc81f3c2a5313194ac847bda9435b
abbasmaryamzoha-gif <abbas_maryam_zoha@student.smc.edu>
2026-07-09 14:32:30 -0700

    added validation rules

--- resources/views/module3/module3b-createform.php
+++ resources/views/module3/module3b-createform.php
@@ -1,3 +1,5 @@
+//GITHUB URL:
+
 
 <form action="" method="GET">
   <label for="name">Full Name:</label>
@@ -17,15 +19,51 @@
   
 
 
-
-
-
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'GET' && !empty($_GET)) {
-  echo "<h3>your message:</h3>";
-  echo "<p>Full Name: " . htmlspecialchars($_GET['name']) . "</p>";
-  echo "<p>Email Address: " . htmlspecialchars($_GET['email']) . "</p>";
-  echo "<p>Topic of Message: " . htmlspecialchars($_GET['topic']) . "</p>";
-  echo "<p>Message:" . htmlspecialchars($_GET['message']) . "</p>";
+//using functions for email and message, since they have restrictions for what can go in them
+
+
+// email validation from module 3a:
+function validateEmail($data) {
+        $retval = filter_var($data, FILTER_SANITIZE_EMAIL);
+        if (!filter_var($retval, FILTER_VALIDATE_EMAIL)) {
+        return "<strong>Not a valid e-mail address.</strong><br />\n";
+    
+    }
+    return($retval);
 }
+
+//check for word count in message:
+function validateMessage($data) {
+    $count = str_word_count($data);
+    if ($count<4 or $count>150) {
+        $retval = "";
+        return "<strong> Message must be between 4 and 150 words.</strong><br />\n";
+        
+    } else {        
+        $retval = trim($data);
+        $retval = stripslashes($retval);
+    }
+
+    return($retval);
+}
+
+
+//page output:
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && !empty($_GET)) {
+
+  echo "<h3>your message:</h3>";
+
+  echo "<p>Full Name: " . htmlspecialchars($_GET['name']) . "</p>";
+
+  echo "<p>Email Address: " . validateEmail(htmlspecialchars($_GET['email'])) . "</p>";
+  
+  echo "<p>Topic of Message: " . htmlspecialchars($_GET['topic']) . "</p>";
+
+  echo "<p>Message:" . validateMessage(htmlspecialchars($_GET['message'])) . "</p>";
+
+  
+}
+
+// when testing, thought it was interesting that you can have multiple of the same field on the same page, and it will accept the last one entered 
 ?>
